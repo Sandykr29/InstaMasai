c65d796e1f9c2fe8d77db09b27a18bed3d78ca9a
sandykr29 <sandeepkumar29774@yahoo.com>
2024-01-15 15:20:50 +0530

    register done

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,9 +1,9 @@
 const express=require("express")
 
 const {UserModel}=require("../model/user.model")
+const bcrypt = require('bcrypt');
 
 const userRouter=express.Router();
-
 
 userRouter.post("/register",async(req,res)=>{
 let {email,password}=req.body;
@@ -11,9 +11,26 @@ try {
     let user=await UserModel.find({email})
     if(user){return res.status(200).send({msg:"User already exist, please login"})}
 
-    let password
+    let passwordReg=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*]).{8,}$/;
+    if(!(passwordReg.test.password)){return res.status(200).send({msg:"Password must contain atleast 1 uppercase, 1 lowercase, 1 digit , 1 special character and must be atleast 8 character long"})}
     
+    const passwordHash=await bcrypt.hash(password,5);
+
+    let newUser=new UserModel({...req.body,password:passwordHash})
+    await newUser.save();
+    res.status(200).send({msg:"New User Added"})
 } catch (error) {
     res.status(400).send({error:error.message})
 }
+})
+
+userRouter.post("/login",async(req,res)=>{
+    let {email,password}=req.body;
+    try {
+        let user=await UserModel.find({email})
+        if(!user){return res.status(200).send({msg:"User does not exist, please register..."})};
+        
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
 })
