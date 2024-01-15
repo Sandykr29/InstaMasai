30f9f96a3627104798ead163341b413d862dc8ff
sandykr29 <sandeepkumar29774@yahoo.com>
2024-01-15 16:59:12 +0530

    f

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const {connection}=require("./db");
+const { userRouter } = require("./route/user.route");
+const { postRouter } = require("./route/post.route");
+const {auth}=require("./middleware/auth.middleware")
+
+const app=express();
+
+app.use(express.json());
+app.use("/users",userRouter)
+app.use("/posts",auth,postRouter)
+
+app.listen(8080,async()=>{
+    try {
+        await connection;
+        console.log("Connected to DB")
+        console.log("Server is running at port 8000")
+    } catch (error) {
+     console.log(error.message)   
+    }
+})
---
+++ route/post.route.js
@@ -0,0 +1,52 @@
+const express=require("express")
+const {PostModel}=require("../model/post.model")
+const postRouter=express.Router();
+
+postRouter.post("/add",async(req,res)=>{
+    try {
+        let post=new PostModel(req.body);
+        await post.save();
+        res.status(200).send({msg:"New post added..."})
+    } catch (error) {
+        res.status(200).send({error:error.message})
+    }
+})
+
+postRouter.get("/",async(req,res)=>{
+    try {
+        let post =await PostModel.find({email:req.body.email});
+        res.status(200).send(post)
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
+})
+
+postRouter.post("/update/:id",async(req,res)=>{
+    let {id}=req.params;
+    try {
+        let post =await PostModel.find({_id:req.body.userID});
+        if(!post){return req.status(200).send({msg:"Invalid Post.."})}
+
+        await PostModel.findByIdAndUpdate({_id:id},req.body);
+        res.status(200).send({msg:"Post updated..."})
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
+})
+
+postRouter.post("/delete/:id",async(req,res)=>{
+    let {id}=req.params;
+    try {
+        let post = await PostModel.findOne({_id:id});
+        if(!post){return req.status(200).send({msg:"Invalid Post.."})}
+
+        await PostModel.findByIdAndDelete({_id:id});
+        res.status(200).send({msg:"Post Deleted..."})
+    } catch (error) {
+        res.status(400).send({error:error.message})
+    }
+})
+
+module.exports={
+    postRouter
+}
